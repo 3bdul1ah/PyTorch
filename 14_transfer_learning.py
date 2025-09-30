a2a3e044ca22124248e9f1e00e933d5dfd029efc
3bdul1ah <a2053231@gmail.com>
2025-09-30 11:09:54 +0400

    14_transfer_learning

--- 14_transfer_learning.py
+++ 14_transfer_learning.py
@@ -1,15 +1,15 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim import lr_scheduler
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
-import time 
+import time
 import os
 import copy
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 mean = np.array([0.5, 0.5, 0.5])
 std = np.array([0.25, 0.25, 0.25])
@@ -29,14 +29,147 @@ data_transforms = {
     ]),
 }
 
-data_dir = r"C:\Users\user\Desktop\PyTorch\data\hymenoptera_data"
-
+data_dir = 'data/hymenoptera_data'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
                   for x in ['train', 'val']}
 
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+dataloader = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
                                              shuffle=True, num_workers=0)
               for x in ['train', 'val']}
+
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(class_names)
+
+
+def imshow(inp, title):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    plt.title(title)
+    plt.show()
+
+dataloader_iterator = iter(dataloader['train'])
+inputs, classes = next(dataloader_iterator)
+
+out = torchvision.utils.make_grid(inputs)
+
+# imshow(out, title=[class_names[x] for x in classes])
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Eporch {epoch}/{num_epochs-1}')
+        print("-" * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else: 
+                model.eval()
+            
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloader[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _,pred = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == 'train':
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(pred == labels.data)
+
+            if phase == 'train':
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects / dataset_sizes[phase]
+
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc {epoch_acc:4f}')
+
+            if phase == 'val':
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+    time_elapsed = time.time() - since 
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val acc: {best_acc:.4f}')
+
+#### Finetuning the convnet ####
+# Load a pretrained model and reset final fully connected layer.
+
+model = models.resnet18(pretrained=True)
+num_ftrs = model.fc.in_features
+
+# fc.weight torch.Size([1000, 512])
+# fc.bias torch.Size([1000])
+
+# after 
+
+# fc.weight torch.Size([2, 512])
+# fc.bias torch.Size([2])
+
+# in is 512 out 1000 
+# print(num_ftrs)
+
+model_conv = torchvision.models.resnet18(pretrained=True)
+# change output of fc to 2 classes (finetune 'transfer learning)
+model.fc = nn.Linear(num_ftrs, 2)
+model.to(device)
+ 
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1) # EVERY 7 EPOCH THE RL IS * BY 10% (Decay)
+
+# for epoch in range(100):
+#     train()
+#     evaulate()
+#     scheduler.step()
+
+# model = train_model(model, criterion, optimizer, scheduler, num_epochs=25)
+
+# --------------------------------------------------------------------------------------------------------
+
+
+#### ConvNet as fixed feature extractor ####
+# Here, we need to freeze all the network except the final layer.
+# We need to set requires_grad == False to freeze the parameters so that the gradients are not computed in backward()
+model_conv = torchvision.models.resnet18(pretrained=True)
+
+for name, param in model_conv.named_parameters():
+    print(name, param.shape)
+
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, 2)
+
+
+# only parameters of final layer are being optimized as
+# opposed to before.
+
+model_conv = model_conv.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+optimizer_conv = optim.lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=25)
